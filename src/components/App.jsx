e34682e98dadd46b8af70a039b3f308a16f3a22c
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-03 22:31:16 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
+import { Routes, Route } from 'react-router-dom';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route />
+    </Routes>
   );
 };
