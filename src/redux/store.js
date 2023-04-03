b83432ff68f23b2f910751cc342dfcc8b92ed46d
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-03 22:27:20 +0300

    Create store.js

---
+++ src/redux/store.js
@@ -0,0 +1,29 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { authReducer } from './auth/authSlice';
+import { ingredientsReducer } from './ingredients/ingrediensSlice';
+import { recipesReducer } from './recipes/recipesSlice';
+import { shoppingListReducer } from './shoppingList/shoppingListSlice';
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
+
+export const store = configureStore({
+  reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
+    ingredients: ingredientsReducer,
+    recepies: recipesReducer,
+    shoppingList: shoppingListReducer,
+  },
+
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
