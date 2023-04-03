ae2bee44bade4650378973eec17fc14e6e682bbd
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-03 22:19:02 +0300

    Create Box.jsx
    add component Box

---
+++ src/components/Box.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+import {
+  position,
+  color,
+  space,
+  layout,
+  flexbox,
+  border,
+  shadow,
+} from 'styled-system';
+
+export const Box = styled.div`
+  ${position}
+  ${color}
+  ${space}
+  ${layout}
+  ${flexbox} 
+  ${border}
+  ${shadow}
+`;
