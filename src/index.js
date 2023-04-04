df405b53ac3896409a8dc1a1aa2938790f7c95bf
Volodymyr Kurov <vokur13@gmail.com>
2023-04-04 09:19:02 +0300

    redux under comment

--- src/index.js
+++ src/index.js
@@ -10,15 +10,15 @@ import { App } from 'components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <ThemeProvider theme={theme}>
-          <BrowserRouter basename="/yummy-frontend">
-            <GlobalStyles />
-            <App />
-          </BrowserRouter>
-        </ThemeProvider>
-      </PersistGate>
-    </Provider>
+    {/* <Provider store={store}> */}
+    {/* <PersistGate loading={null} persistor={persistor}> */}
+    <ThemeProvider theme={theme}>
+      <BrowserRouter basename="/yummy-frontend">
+        <GlobalStyles />
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+    {/* </PersistGate> */}
+    {/* </Provider> */}
   </React.StrictMode>
 );
