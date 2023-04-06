7e7936d141abbe513ca63744b878477190583580
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-06 23:46:53 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,9 @@ import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { authReducer } from './auth/authSlice';
-import { ingredientsReducer } from './ingredients/ingrediensSlice';
-import { recipesReducer } from './recipes/recipesSlice';
-import { shoppingListReducer } from './shoppingList/shoppingListSlice';
+// import { ingredientsReducer } from './ingredients/ingrediensSlice';
+// import { recipesReducer } from './recipes/recipesSlice';
+// import { shoppingListReducer } from './shoppingList/shoppingListSlice';
 
 const authPersistConfig = {
   key: 'auth',
