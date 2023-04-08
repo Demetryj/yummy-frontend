2732be35e211fe0125d39a88fcb0cc761049df8f
MaryKotliar <glinskayam@ukr.net>
2023-04-08 23:05:40 +0300

    Finished Hero

--- src/components/RecipePageHero/RecipePageHero.styled.js
+++ src/components/RecipePageHero/RecipePageHero.styled.js
@@ -5,20 +5,17 @@ import recipeTable from 'images/recipePage/recipe_tabl.jpg';
 import recipeTable2 from 'images/recipePage/recipe_tabl@2x.jpg';
 import recipeDesk from 'images/recipePage/recipe_desk.jpg';
 import recipeDesk2 from 'images/recipePage/recipe_desk@2x.jpg';
+import { FiClock } from 'react-icons/fi';
 
 export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
   align-items: center;
-  margin-left: auto;
-  margin-right: auto;
-  padding-top: 144px;
-  padding-bottom: 90px;
-  padding-left: 38px;
-  padding-right: 38px;
-  width: 375px;
-  height: 100vh;
+  margin-left: 0 auto;
+  padding-top: ${p => p.theme.spacing(72)};
+  padding-bottom: ${p => p.theme.spacing(45)};
+  padding-left: ${p => p.theme.spacing(19)};
+  padding-right: ${p => p.theme.spacing(19)};
+  width: 100%;
+
   background-image: url(${recipeMob});
   background-size: cover;
   @media (min-device-pixel-ratio: 2),
@@ -28,12 +25,11 @@ export const Container = styled.div`
     background-image: url(${recipeMob2});
   }
   @media screen and (min-width: 768px) {
-    padding-bottom: 32 px;
-    padding-left: 132px;
-    padding-right: 132px;
-    padding-top: 136px;
-    /* padding: ${p => p.theme.spacing(171)} ${p => p.theme.spacing(62)}; */
-    width: 768px;
+    padding-bottom: ${p => p.theme.spacing(16)};
+    padding-left: ${p => p.theme.spacing(66)};
+    padding-right: ${p => p.theme.spacing(66)};
+    padding-top: ${p => p.theme.spacing(68)};
+    max-width: 768px;
     background-image: url(${recipeTable});
     @media (min-device-pixel-ratio: 2),
       (-webkit-min-device-pixel-ratio: 2),
@@ -43,12 +39,10 @@ export const Container = styled.div`
     }
   }
   @media screen and (min-width: 1440px) {
-    padding-bottom: 32 px;
-    padding-left: 392px;
-    padding-right: 392px;
-    padding-top: 164px;
-    /* padding: ${p => p.theme.spacing(107.5)} ${p => p.theme.spacing(220)}; */
-    width: 1440px;
+    padding-left: ${p => p.theme.spacing(196)};
+    padding-right: ${p => p.theme.spacing(196)};
+    padding-top: ${p => p.theme.spacing(82)};
+    max-width: 1600px;
     background-image: url(${recipeDesk});
     @media (min-device-pixel-ratio: 2),
       (-webkit-min-device-pixel-ratio: 2),
@@ -56,5 +50,59 @@ export const Container = styled.div`
       (min-resolution: 2dppx) {
       background-image: url(${recipeDesk2});
     }
+  }
+`;
+export const Title = styled.h3`
+  font-weight: ${p => p.theme.fontWeights.semiBold};
+  color: ${p => p.theme.colors.greenTextColor};
+  font-size: ${p => p.theme.fontSizes.l};
+  text-align: center;
+  @media screen and (min-width: 768px) {
+    font-size: ${p => p.theme.fontSizes.sb};
+  }
+
+  @media screen and(min-width: 1440px) {
+    font-size: ${p => p.theme.fontSizes.sb};
+  }
+`;
+export const Text = styled.p`
+  font-weight: ${p => p.theme.fontWeights.regular};
+  color: ${p => p.theme.colors.blackTextColor};
+  font-size: ${p => p.theme.fontSizes.xs};
+  line-height: 1.33;
+  text-align: center;
+  margin-top: ${p => p.theme.spacing(9)};
+  margin-bottom: ${p => p.theme.spacing(12)};
+  @media screen and (min-width: 768px) {
+    margin-top: ${p => p.theme.spacing(12)};
+    font-size: ${p => p.theme.fontSizes.xm};
+  }
+
+  @media screen and(min-width: 1440px) {
+    font-size: ${p => p.theme.fontSizes.xm};
+  }
+`;
+export const ClockBlock = styled.div`
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  margin-top: ${p => p.theme.spacing(21)};
+`;
+export const Time = styled.p`
+  font-weight: ${p => p.theme.fontWeights.medium};
+  color: ${p => p.theme.colors.blackTextColor};
+  font-size: ${p => p.theme.fontSizes.xs};
+  margin-left: ${p => p.theme.spacing(3)};
+  @media screen and (min-width: 768px) {
+    margin-left: ${p => p.theme.spacing(5)};
+    font-size: ${p => p.theme.fontSizes.s};
+  }
+`;
+export const Clock = styled(FiClock)`
+  width: 14px;
+  height: 14px;
+  @media screen and (min-width: 768px) {
+    width: 20px;
+    height: 20px;
   }
 `;
--- src/redux/recipes/recipesSlice.js
+++ src/redux/recipes/recipesSlice.js
@@ -30,10 +30,7 @@ const recipesSlice = createSlice({
         state.error = action.payload;
       })
       .addCase(addToFavorites.fulfilled, (state, action) => {
-        // state.items = state.items.push(action.payload);
         state.items.map(item => item.id === action.payload.id);
-
-        // recipe.push(action.payload.id);
 
         state.isLoading = false;
 
@@ -62,47 +59,5 @@ const recipesSlice = createSlice({
       });
   },
 });
-
-// });t recipesSlice = createSlice({
-//   name: 'recipes',
-//   initialState,
-//   extraReducers: {
-//     [fetchRecipes.pending]: handlePending,
-//     [fetchRecipes.fulfilled](state, action) {
-//       state.isLoading = false;
-//       state.error = null;
-//       state.items = action.payload;
-//     },
-//     [fetchRecipes.rejected]: handleRejected,
-//     [addRecipe.pending]: handlePending,
-//     [addRecipe.fulfilled](state, action) {
-//       state.isLoading = false;
-//       state.error = null;
-//       state.items.push(action.payload);
-//     },
-//     [addRecipe.rejected]: handleRejected,
-//     [deleteRecipe.pending]: handlePending,
-//     [deleteRecipe.fulfilled](state, action) {
-//       state.isLoading = false;
-//       state.error = null;
-//       const index = state.recipes.findIndex(
-//         contact => contact.id === action.payload.id
-//       );
-//       state.recipes.splice(index, 1);
-//     },
-//     [deleteRecipe.rejected]: handleRejected,
-// [getRecipeById.pending]: handlePending,
-// [getRecipeById.fulfilled](state, action) {
-//   state.isLoading = false;
-//   state.error = null;
-
-//   state.items.map(recipe => recipe.id === action.payload.id);
-// },
-// [getRecipeById.rejected]: handleRejected,
-
-// extraReducers: builder => {
-//   builder.addCase();
-// },
-// });
 
 export const recipesReducer = recipesSlice.reducer;
