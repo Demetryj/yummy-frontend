e151a83c9fee1e380de409be172ab745e6dddfd0
MaryKotliar <glinskayam@ukr.net>
2023-04-09 15:42:43 +0300

    update Recipe_Page block

--- src/components/MainTitle/MainTitle.styled.js
+++ src/components/MainTitle/MainTitle.styled.js
@@ -1,16 +1,16 @@
 import styled from 'styled-components';
 
-// export const Title = styled.h3`
-//   font-family: 'Poppins';
-//   font-style: normal;
-//   font-weight: ${p => p.theme.fontWeights.semiBold};
-//   color: ${p => p.theme.colors.titleTextColor};
-//   font-size: ${p => p.theme.fontSizes.xl};
-//   @media screen and ${p => p.theme.media.medium} {
-//     font-size: ${p => p.theme.fontSizes.xxl};
-//   }
+export const Title = styled.h3`
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: ${p => p.theme.fontWeights.semiBold};
+  color: ${p => p.theme.colors.titleTextColor};
+  font-size: ${p => p.theme.fontSizes.xl};
+  @media screen and ${p => p.theme.media.medium} {
+    font-size: ${p => p.theme.fontSizes.xxl};
+  }
 
-//   @media screen and ${p => p.theme.media.large} {
-//     font-size: ${p => p.theme.fontSizes.sb};
-//   }
-// `;
+  @media screen and ${p => p.theme.media.large} {
+    font-size: ${p => p.theme.fontSizes.sb};
+  }
+`;
