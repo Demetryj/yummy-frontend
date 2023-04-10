372c36ae574c7c91ed900454f1f8972f09d748cf
MaryKotliar <glinskayam@ukr.net>
2023-04-10 15:37:40 +0300

    update

--- src/redux/recipes/operations.js
+++ src/redux/recipes/operations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://soyummy-pg-2.onrender.com/api';
+axios.defaults.baseURL = 'http:localhost:3001';
 
 export const fetchRecipesMainPage = createAsyncThunk(
   'recipes/fetchPopular',
