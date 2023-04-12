59b0d94ed625b2ed03a3bb859df061247d9fa4d9
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-12 20:16:00 +0000

    Deploying to gh-pages from @ Demetryj/yummy-frontend@5e63859924b011cd884f5599290f9a96672cb6ac 🚀

--- static/js/607.fe0e3b78.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[607],{5607:function(e,t,r){r.r(t);var a=r(3329);t.default=function(){return(0,a.jsx)("main",{children:(0,a.jsx)("h1",{children:"Recipe Page"})})}}}]);
-//# sourceMappingURL=607.fe0e3b78.chunk.js.map
