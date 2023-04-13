ef8b46b937dab485ecce2671476e871900d21922
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-13 12:27:38 +0300

    Update selectors.js

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -5,12 +5,8 @@ export const selectUser = state => state.auth.user;
 
 export const selectIsRefreshing = state => state.auth.isRefreshing;
 
-
 export const selectToken = state => state.auth.token;
-
-
 
 export const selectIsLoading = state => state.auth.isLoading;
 
 export const selectError = state => state.auth.errorMessage;
-
