18b3162a29c4ec8991fd2a4a63b4c346ecb257e4
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-13 12:27:00 +0300

    Update WelcomeMain.jsx
    add section

--- src/components/WelcomeMain/WelcomeMain.jsx
+++ src/components/WelcomeMain/WelcomeMain.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import {
+  Section,
   Container,
   Logo,
   Title,
@@ -11,22 +12,24 @@ import logo from 'images/logo/logo-header.svg';
 
 export const WelcomeMain = () => {
   return (
-    <Container>
-      <Logo src={logo} alt="logo" />
-      <Title>Welcome to the App!</Title>
-      <Text>
-        This app offers more than just a collection of recipes - it is designed
-        to be your very own digital cookbook. You can easily save and retrieve
-        your own recipes at any time.
-      </Text>
-      <div>
-        <Link to="/register">
-          <BtnRegister type="button">Registration</BtnRegister>
-        </Link>
-        <Link to="/signin">
-          <BtnSignin type="button">Sign in</BtnSignin>
-        </Link>
-      </div>
-    </Container>
+    <Section>
+      <Container>
+        <Logo src={logo} alt="logo" />
+        <Title>Welcome to the App!</Title>
+        <Text>
+          This app offers more than just a collection of recipes - it is
+          designed to be your very own digital cookbook. You can easily save and
+          retrieve your own recipes at any time.
+        </Text>
+        <div>
+          <Link to="/register">
+            <BtnRegister type="button">Registration</BtnRegister>
+          </Link>
+          <Link to="/signin">
+            <BtnSignin type="button">Sign in</BtnSignin>
+          </Link>
+        </div>
+      </Container>
+    </Section>
   );
 };
