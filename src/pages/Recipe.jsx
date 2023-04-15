a92d020a72961f3523ba170a9a1affd64d291746
MaryKotliar <glinskayam@ukr.net>
2023-04-15 17:27:17 +0300

    update
    update	modified:   src/pages/Recipe.jsx

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -10,41 +10,32 @@ import {
 import { Loader } from 'components/Loader/Loader.jsx';
 import { onScrollUp } from 'utils/scrollUp';
 
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getRecipeById } from 'redux/recipes/operations';
 const Recipe = () => {
   const { recipeId } = useParams();
-  console.log(recipeId);
-  const [recipe, setRecipe] = useState({});
-  const recipeData = useSelector(selectRecipes);
+
+  const recipes = useSelector(selectRecipes);
+  const error = useSelector(selectError);
+  const isLoading = useSelector(selectIsLoading);
   const dispatch = useDispatch();
+  console.log(recipes);
+  console.log(recipeId);
   useEffect(() => {
     dispatch(getRecipeById(recipeId));
     onScrollUp();
   }, [dispatch, recipeId]);
-  console.log(recipeId);
-  const error = useSelector(selectError);
-  const isLoading = useSelector(selectIsLoading);
-  console.log(recipeData);
 
-  useEffect(() => {
-    setRecipe({ ...recipeData[0] });
-  }, [recipeData]);
-
-  return (
-    <>
-      {Object.keys(recipe).length !== 0 && (
-        <>
-          <RecipePageHero recipe={recipe} />
-          <RecipeIngredientList recipe={recipe} />
-          <RecipePreparation recipe={recipe} />
-        </>
-      )}
-      {isLoading && !error && !recipe && <Loader />}
-      {error && <p>Something is wrong. Please try again later.</p>}
-    </>
-  );
+  // return (
+  //   <>
+  //     {recipe && <RecipePageHero recipe={recipe} />}
+  //     {recipe && <RecipeIngredientList recipe={recipe} />}
+  //     {recipe && <RecipePreparation recipe={recipe} />}
+  //     {isLoading && !error && !recipe && <Loader />}
+  //     {error && <p>Something is wrong. Please try again later.</p>}
+  //   </>
+  // );
 };
 
 export default Recipe;
