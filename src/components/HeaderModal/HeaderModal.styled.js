07e9358c4fc647f25e49deafb2eae9f04783e5c9
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-16 23:32:08 +0300

    Update HeaderModal.styled.js

--- src/components/HeaderModal/HeaderModal.styled.js
+++ src/components/HeaderModal/HeaderModal.styled.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const ModalBackdrop = styled.div`
   position: fixed;
+  z-index: 25;
   top: 0;
   left: 0;
   width: 100vw;
@@ -15,7 +16,7 @@ export const ModalContent = styled.div`
   top: 0;
   left: 0;
   width: 100%;
-  height: 100%
+  height: 100%;
   padding: 12px;
-  background-color: #EBF3D4;
+  background-color: #ebf3d4;
 `;
