0f5a4f782b1de98f81907434755a0496cf68fd98
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-16 23:00:50 +0300

    Update RecipeCard.jsx

--- src/components/RecipesPhotosList/RecipeCard.jsx
+++ src/components/RecipesPhotosList/RecipeCard.jsx
@@ -2,8 +2,7 @@ import { forwardRef } from 'react';
 import { Image, ImageItemLink, Lable } from './RecipesPhotoList.styled';
 
 export const RecipeCard = forwardRef(({ recipe }, ref) => {
-  console.log(recipe._id);
-  console.log(recipe);
+ 
   const ItemBody = (
     <ImageItemLink to={`/recipe/${recipe._id}`}>
       <Image src={`${recipe.thumb}`} />
