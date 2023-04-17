9f4de3ff4e73a3ca2e6818ce92727b19195759a6
serhii_dem <realsergeiii0@gmail.com>
2023-04-17 19:14:02 +0300

    fixxxxxxxxxxxx

--- src/components/RecipesPhotosList/RecipesPhotosList.jsx
+++ src/components/RecipesPhotosList/RecipesPhotosList.jsx
@@ -15,7 +15,6 @@ export const RecipesPhotosList = () => {
   const [page, setPage] = useState(1);
   const [category, setCategory] = useState('');
   const [recipes, setRecipes] = useState([]);
-
   const [hasNextPage, setHasNextPage] = useState(false);
   const isFirstRender = useRef(category);
   const limit = 8;
@@ -58,6 +57,7 @@ export const RecipesPhotosList = () => {
 
   return (
     <>
+      {' '}
       {recipes && recipes.length !== 0 && (
         <PhotosList>
           {recipes.map((recipe, i) =>
