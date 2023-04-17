6b7d98da95bd4adb267635a0d0c33cc08234ddd7
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-04-17 19:00:39 +0300

    Create Title.jsx

---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,9 @@
+import { TitleConteiner } from './Title.styled';
+
+export const Title = ({ children }) => {
+  return (
+    <TitleConteiner>
+      <h2>{children}</h2>
+    </TitleConteiner>
+  );
+};
