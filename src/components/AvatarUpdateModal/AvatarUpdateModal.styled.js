4dca06616c2e44b6c63e8bd496bed651fc987d37
Volodymyr Kurov <vokur13@gmail.com>
2023-04-17 18:56:29 +0300

    Update AvatarUpdateModal.styled.js

--- src/components/AvatarUpdateModal/AvatarUpdateModal.styled.js
+++ src/components/AvatarUpdateModal/AvatarUpdateModal.styled.js
@@ -19,18 +19,16 @@ export const Content = styled.div`
   background: ${p => p.theme.colors.whiteColor};
   border-radius: 24px;
   box-shadow: 0px 4px 48px rgba(0, 0, 0, 0.1);
-  top: 2%;
+  top: 50%;
   left: 50%;
-  transform: translate(-50%);
+  transform: translate(-50%, -50%);
 
   @media screen and ${p => p.theme.media.medium} {
     width: ${p => p.theme.spacing(240)};
-    top: ${p => p.theme.spacing(39)};
   }
 
   @media screen and ${p => p.theme.media.large} {
     width: ${p => p.theme.spacing(250)};
-    top: ${p => p.theme.spacing(39)};
     padding-top: ${p => p.theme.spacing(30)};
     padding-bottom: ${p => p.theme.spacing(30)};
     padding-left: ${p => p.theme.spacing(25)};
