66121a9aceb21ef2bc2252634fdad4444e40abee
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-04-27 17:07:17 +0300

    Update RecipeItem.styled.js

--- src/components/Recipe/RecipeItem.styled.js
+++ src/components/Recipe/RecipeItem.styled.js
@@ -13,14 +13,14 @@ export const Card = styled.div`
       ? p.theme.colors.bcgWhiteColor
       : p.theme.colors.bcgWhiteColor};
 
-  padding: ${p =>
-    p.location === 'favorite' ? '14px 18px 14px 14px' : '14px 9px'};
+  padding: ${p => (p.location === 'favorite' ? '14px 9px' : '14px 9px')};
 
   border-radius: ${p => p.theme.radii.normal};
 
   @media screen and (${p => p.theme.media.medium}) {
     height: 288px;
     width: 704px;
+
     gap: ${p => (p.location === 'favorite' ? '24px' : '24px')};
     padding: ${p => (p.location === 'favorite' ? '28px 24px' : '28px 24px')};
   }
@@ -61,21 +61,11 @@ export const Information = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
+
   flex-grow: 1;
 
-  border-radius: ${p =>
-    p.location === 'recipes' ? p.theme.radii.normal : 'none'};
-
-  background-color: ${p =>
-    p.location === 'recipes' ? p.theme.colors.bcgWhiteColor : 'transparent'};
-
   @media screen and (${p => p.theme.media.medium}) {
-    padding: ${p => (p.location === 'recipes' ? '28px' : '0px')};
-  }
-
-  @media screen and (${p => p.theme.media.large}) {
-    padding: ${p => (p.location === 'recipes' ? '40px' : '0px')};
+    justify-content: unset;
   }
 `;
 
@@ -98,14 +88,16 @@ export const Title = styled.h3`
 
   max-width: calc(80% - 18px);
   height: fit-content;
-  font-family: ${p => p.theme.fonts.body};
-  font-size: ${p => p.theme.fontSizes.s};
+
   font-weight: ${p => p.theme.fontWeights.medium};
+  font-size: ${p => p.theme.fontSizes.s};
   letter-spacing: ${p => p.theme.letterSpacings.subheader};
+
   color: ${p => p.theme.colors.subtitleTextColor};
 
   @media screen and (${p => p.theme.media.medium}) {
     max-width: 300px;
+
     font-size: ${p => p.theme.fontSizes.l};
   }
 
@@ -115,20 +107,23 @@ export const Title = styled.h3`
 `;
 
 export const Description = styled.div`
-  width: 100%;
-  height: 50%;
+  width: ${p => (p.location === 'favorite' ? '155px' : '163px')};
+  height: ${p => (p.location === 'favorite' ? '57px' : '51px')};
+
   margin-top: ${p => (p.location === 'favorite' ? '14px' : '14px')};
   margin-bottom: ${p => (p.location === 'favorite' ? '25px' : '12px')};
+
   text-overflow: ellipsis;
-  font-family: ${p => p.theme.fonts.body};
-  font-size: 8px;
+
+  font-size: ${p => p.theme.fontSizes.xxs};
   font-weight: ${p => p.theme.fontWeights.regular};
-  letter-spacing: ${p => p.theme.letterSpacings.content};
   line-height: ${p => p.theme.lineHeights.description};
+  letter-spacing: ${p => p.theme.letterSpacings.content};
+
   color: ${p => p.theme.colors.mainTextColor};
 
   & span {
-    max-width: 170px;
+    max-width: 100%;
     -webkit-box-orient: vertical;
     display: -webkit-box;
     -webkit-line-clamp: 5;
@@ -139,31 +134,34 @@ export const Description = styled.div`
     hyphens: auto;
 
     @media screen and (${p => p.theme.media.medium}) {
-      max-width: 390px;
+      max-width: 100%;
     }
 
     @media screen and (${p => p.theme.media.large}) {
-      max-width: 680px;
+      max-width: 100%;
     }
   }
 
   @media screen and (${p => p.theme.media.medium}) {
+    width: ${p => (p.location === 'favorite' ? '298px' : '404px')};
+    height: ${p => (p.location === 'favorite' ? '73px' : '107px')};
+
+    margin-top: ${p => (p.location === 'favorite' ? '18px' : '14px')};
+    margin-bottom: ${p => (p.location === 'favorite' ? '58px' : '28px')};
+
     font-size: ${p => p.theme.fontSizes.s};
     line-height: ${p => p.theme.lineHeights.extraContent};
-    margin-top: ${p => (p.location === 'favorite' ? '28px' : '14px')};
-    margin-bottom: ${p => (p.location === 'favorite' ? '58px' : '12px')};
   }
 
   @media screen and (${p => p.theme.media.large}) {
-    margin-top: 30px 0 25px;
+    width: ${p => (p.location === 'favorite' ? '679px' : '100%')};
+    height: ${p => (p.location === 'favorite' ? '158px' : '134px')};
+
+    margin-top: ${p => (p.location === 'favorite' ? '30px' : '20px')};
+    margin-bottom: ${p => (p.location === 'favorite' ? '38px' : '67px')};
+
     font-size: ${p => p.theme.fontSizes.xm};
     line-height: ${p => p.theme.lineHeights.content};
-
-    & br {
-      display: block;
-      content: '';
-      margin-bottom: 14px;
-    }
   }
 `;
 
@@ -177,11 +175,9 @@ export const TimeWrapper = styled.div`
 export const Time = styled.p`
   display: inline-flex;
 
-  font-family: ${p => p.theme.fonts.body};
   font-weight: ${p => p.theme.fonts.medium};
-  font-size: 10px;
+  font-size: ${p => p.theme.fontSizes.x};
   line-height: ${p => p.theme.lineHeights.time};
-
   letter-spacing: ${p => p.theme.letterSpacings.subheader};
 
   color: ${p => p.theme.colors.subtitleTextColor};
