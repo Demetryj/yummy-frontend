df6217400db2d00baa452dd2ddfa6ee10a3e87e3
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-04-27 17:06:41 +0300

    Update Container.styled.js

--- src/components/Container/Container.styled.js
+++ src/components/Container/Container.styled.js
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 
 export const MainConteiner = styled.div`
-  padding: 0 16px;
-  background-color: ${p => p.theme.colors.lightBgColor};
+  padding: 0;
+
   @media screen and (${p => p.theme.media.medium}) {
-    padding: 0 32px;
+    padding: 0;
   }
 
   @media screen and (${p => p.theme.media.large}) {
-    padding: 0 100px;
+    padding: 0;
   }
 `;
