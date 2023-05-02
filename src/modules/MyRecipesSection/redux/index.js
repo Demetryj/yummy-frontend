ef24d1b0454deb4f149d7af1ff36e1c989703bb8
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-05-02 14:45:54 +0300

    Create index.js

---
+++ src/modules/MyRecipesSection/redux/index.js
@@ -0,0 +1,8 @@
+export {  getRecipes, removeRecipe } from './operations';
+export { myRecipesReducer } from './slice';
+export {
+  selectRecipes,
+  selectTotal,
+  selectIsLoading,
+  selectError,
+} from './selectors';
