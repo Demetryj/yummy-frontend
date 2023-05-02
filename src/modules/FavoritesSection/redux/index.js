582b5001c1fdb75a83f5ba76d9aabf2661f5f7b2
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-05-02 14:46:21 +0300

    Create index.js

---
+++ src/modules/FavoritesSection/redux/index.js
@@ -0,0 +1,8 @@
+export {  getFavorites, removeFavorite } from './operations';
+export { favoritesReducer } from './slice';
+export {
+  selectRecipes,
+  selectTotal,
+  selectIsLoading,
+  selectError,
+} from './selectors';
