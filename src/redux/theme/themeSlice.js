e167de0b61cb9fe4a2b497a460f6b0cede7ef69f
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-05-17 22:04:54 +0300

    Update themeSlice.js
    change name variable

--- src/redux/theme/themeSlice.js
+++ src/redux/theme/themeSlice.js
@@ -3,11 +3,11 @@ import { createSlice } from '@reduxjs/toolkit';
 const themeSlice = createSlice({
   name: 'theme',
   initialState: {
-    theme: 'light',
+    mode: 'light',
   },
   reducers: {
     toggleTheme(state, action) {
-      state.theme = action.payload;
+      state.mode = action.payload;
     },
   },
 });
