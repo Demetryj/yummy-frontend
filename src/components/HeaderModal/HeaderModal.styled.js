e24c0eade8f4fc8a8ced38384a4806932c85e293
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-05-17 22:08:32 +0300

    Update HeaderModal.styled.js
    add change background-color when switching theme

--- src/components/HeaderModal/HeaderModal.styled.js
+++ src/components/HeaderModal/HeaderModal.styled.js
@@ -3,6 +3,7 @@ import BgMob from 'images/spinach/mobile/spinach-menu-mob.png';
 import BgMob2x from 'images/spinach/mobile/spinach-menu-mob@2x.png';
 import BgTabl from 'images/spinach/tablet/spinach-menu-tabl.png';
 import BgTabl2x from 'images/spinach/tablet/spinach-menu-tabl@2x.png';
+
 export const ModalBackdrop = styled.div`
   position: fixed;
   z-index: 25;
@@ -17,11 +18,14 @@ export const ModalBackdrop = styled.div`
 export const ModalContent = styled.div`
   width: 100%;
   height: 100%;
-  background-color: #ebf3d4;
+  background-color: ${p =>
+    p.mode === 'light'
+      ? p.theme.colors.bgMobileMenu
+      : p.theme.colors.bgMainDark};
   background-image: url(${BgMob});
   background-position: bottom right;
   background-repeat: no-repeat;
-  background-size: contain;
+  background-size: 360px;
 
   @media (min-device-pixel-ratio: 2),
     (min-resolution: 192dpi),
@@ -31,6 +35,7 @@ export const ModalContent = styled.div`
 
   @media screen and (min-width: 768px) {
     background-image: url(${BgTabl});
+    background-size: 558px;
 
     @media (min-device-pixel-ratio: 2),
       (min-resolution: 192dpi),
