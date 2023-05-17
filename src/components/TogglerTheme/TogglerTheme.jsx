e209c391b2048189341068ba38291c4b1425095d
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-05-17 18:12:34 +0300

    Create TogglerTheme.jsx

---
+++ src/components/TogglerTheme/TogglerTheme.jsx
@@ -0,0 +1,29 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleTheme } from 'redux/theme/themeSlice';
+import { selectTheme } from 'redux/theme/selectors';
+import { Switcher, Checkbox, Label, Ball } from './TogglerTheme.styled';
+
+export const TogglerTheme = ({ device }) => {
+  const dispatch = useDispatch();
+  const { theme } = useSelector(selectTheme);
+
+  const onChangeTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+
+    dispatch(toggleTheme(newTheme));
+  };
+
+  return (
+    <Switcher device={device}>
+      <Checkbox
+        type="checkbox"
+        checked={theme === 'dark'}
+        id="theme-switcher"
+        onChange={onChangeTheme}
+      />
+      <Label mode={theme} htmlFor="theme-switcher">
+        <Ball />
+      </Label>
+    </Switcher>
+  );
+};
