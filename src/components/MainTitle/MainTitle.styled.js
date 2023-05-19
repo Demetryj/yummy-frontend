8f3666ff76102aaa79347312ccf2a5098bc40a34
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-05-19 21:09:26 +0300

    Update MainTitle.styled.js
    add change color of Title when changing theme

--- src/components/MainTitle/MainTitle.styled.js
+++ src/components/MainTitle/MainTitle.styled.js
@@ -4,7 +4,10 @@ export const Title = styled.h3`
   font-family: 'Poppins';
   font-style: normal;
   font-weight: ${p => p.theme.fontWeights.semiBold};
-  color: ${p => p.theme.colors.titleTextColor};
+  color: ${p =>
+    p.mode === 'light'
+      ? p.theme.colors.titleTextColor
+      : p.theme.colors.whiteTextColor};
   font-size: ${p => p.theme.fontSizes.xl};
 
   @media screen and (${p => p.theme.media.medium}) {
