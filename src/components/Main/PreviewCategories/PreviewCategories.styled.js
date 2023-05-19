692a75d48f17050614d9f1747c4a55aacd15be49
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-05-19 21:12:38 +0300

    Update PreviewCategories.styled.js
    add change color  when changing theme

--- src/components/Main/PreviewCategories/PreviewCategories.styled.js
+++ src/components/Main/PreviewCategories/PreviewCategories.styled.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
 
 export const ListCategory = styled.ul`
   display: flex;
@@ -7,12 +7,13 @@ export const ListCategory = styled.ul`
   gap: ${p => p.theme.spacing(16)};
 
   @media screen and (${p => p.theme.media.medium}) {
-    gap: ${p => p.theme.spacing(25)}  
-}
+    gap: ${p => p.theme.spacing(25)};
+  }
 
   @media screen and (${p => p.theme.media.large}) {
-    gap: ${p => p.theme.spacing(50)}  
-}`
+    gap: ${p => p.theme.spacing(50)};
+  }
+`;
 
 export const ListCard = styled.ul`
   margin-top: ${p => p.theme.spacing(16)};
@@ -22,36 +23,39 @@ export const ListCard = styled.ul`
   gap: ${p => p.theme.spacing(0)};
 
   @media screen and (${p => p.theme.media.medium}) {
-    margin-top:${p => p.theme.spacing(20)};
+    margin-top: ${p => p.theme.spacing(20)};
     margin-bottom: ${p => p.theme.spacing(20)};
     gap: ${p => p.theme.spacing(16)};
-}
+  }
 
   @media screen and (${p => p.theme.media.large}) {
     margin-top: ${p => p.theme.spacing(25)};
-    margin-bottom:${p => p.theme.spacing(25)};
+    margin-bottom: ${p => p.theme.spacing(25)};
     gap: ${p => p.theme.spacing(6.5)};
-}`
+  }
+`;
 
 export const WrapRecipe = styled.div`
   width: ${p => p.theme.spacing(171.5)};
 
   @media screen and (${p => p.theme.media.medium}) {
     width: ${p => p.theme.spacing(168)};
-}
+  }
 
-  @media screen and (${p => p.theme.media.large}){
+  @media screen and (${p => p.theme.media.large}) {
     width: ${p => p.theme.spacing(150)};
-}`
+  }
+`;
 
 export const Wrap = styled.div`
-text-align: right;`
+  text-align: right;
+`;
 
 export const ButtonSeeAll = styled.button`
-  padding:${p => p.theme.spacing(5)} ${p => p.theme.spacing(12)};
+  padding: ${p => p.theme.spacing(5)} ${p => p.theme.spacing(12)};
   border-radius: ${p => p.theme.radii.light};
-  color:${p => p.theme.colors.whiteColor};
-  background:${p => p.theme.colors.greenTextColor};
+  color: ${p => p.theme.colors.whiteColor};
+  background: ${p => p.theme.colors.greenTextColor};
   font-weight: ${p => p.theme.fontWeights.regular};
   font-family: inherit;
   cursor: pointer;
@@ -59,11 +63,13 @@ export const ButtonSeeAll = styled.button`
   &:hover,
   &:focus {
     background-color: ${p => p.theme.colors.btnDarckBgColor};
-}`
+  }
+`;
 
 export const LinkDown = styled(Link)`
-  display: block; 
-  text-align: center;`
+  display: block;
+  text-align: center;
+`;
 
 export const ButtonDown = styled.button`
   margin-top: ${p => p.theme.spacing(20)};
@@ -75,11 +81,13 @@ export const ButtonDown = styled.button`
   font-size: ${p => p.theme.fontSizes.s};
   line-height: 1.5;
 
-  color: ${p => p.theme.colors.yummyColor};
+  color: ${p =>
+    p.mode === 'light'
+      ? p.theme.colors.yummyColor
+      : p.theme.colors.whiteTextColor};
 
   border: ${p => p.theme.borders.medium};
   border-color: ${p => p.theme.colors.greenTextColor};
-  /* clip-path: inset(25% 0 25% 0 round 15% 35% 15% 30%); */
   border-radius: 35px 70px;
 
   background-color: transparent;
@@ -96,4 +104,5 @@ export const ButtonDown = styled.button`
     padding: ${p => p.theme.spacing(10)} ${p => p.theme.spacing(21)};
 
     font-size: ${p => p.theme.fontSizes.m};
-  }`
+  }
+`;
