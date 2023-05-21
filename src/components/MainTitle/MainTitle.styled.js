79698786a68d8217a3691a185f592a81326eb484
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-05-21 16:28:03 +0300

    Update MainTitle.styled.js
    change title

--- src/components/MainTitle/MainTitle.styled.js
+++ src/components/MainTitle/MainTitle.styled.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Title = styled.h3`
+export const Title = styled.h2`
   font-family: 'Poppins';
   font-style: normal;
   font-weight: ${p => p.theme.fontWeights.semiBold};
