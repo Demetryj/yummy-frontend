64681b353f8b3f5312aa7e8b6e7551b363eec88a
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-05-21 21:25:16 +0300

    Update Container.styled.js
    add padding-top

--- src/components/Container/Container.styled.js
+++ src/components/Container/Container.styled.js
@@ -1,13 +1,16 @@
 import styled from 'styled-components';
 
 export const MainConteiner = styled.div`
-  padding: 0;
+  /* padding: 0; */
+  padding-top: ${p => p.theme.spacing(25)};
 
   @media screen and (${p => p.theme.media.medium}) {
-    padding: 0;
+    /* padding: 0; */
+    padding-top: ${p => p.theme.spacing(28)};
   }
 
   @media screen and (${p => p.theme.media.large}) {
-    padding: 0;
+    /* padding: 0; */
+    padding-top: ${p => p.theme.spacing(42)};
   }
 `;
