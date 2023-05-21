aa765073657019ba95d37aa57a4f079c7654804f
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-05-21 16:28:55 +0300

    Update Ingredient.jsx
    add change theme

--- src/components/IngredientsShoppingList/Ingredient/Ingredient.jsx
+++ src/components/IngredientsShoppingList/Ingredient/Ingredient.jsx
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteIngredientFromShoppingList } from 'redux/shoppingList/operations';
 import { selectError } from 'redux/shoppingList/selectors';
+import { selectTheme } from 'redux/theme/selectors';
 import defaultImage from 'images/plugs/apple 93x97@2x.png';
 import {
   Product,
@@ -16,6 +17,7 @@ import * as icons from './icons';
 export const Ingredient = ({ id, title, thumb, measure }) => {
   const dispatch = useDispatch();
   const error = useSelector(selectError);
+  const theme = useSelector(selectTheme);
 
   function handleDelete(id) {
     if (!id || error) {
@@ -33,11 +35,14 @@ export const Ingredient = ({ id, title, thumb, measure }) => {
   return (
     <Product>
       <ProductPicture src={thumb || defaultImage} alt={title} />
-      <ProductName>{title || 'Product'}</ProductName>
+      <ProductName mode={theme}>{title || 'Product'}</ProductName>
 
       <ProductQuantity>{measure || 1}</ProductQuantity>
       <Button type="button" onClick={() => handleDelete(id)}>
-        <icons.RxCross2 size={25} />
+        <icons.RxCross2
+          size={25}
+          color={theme === 'light' ? '#22252A' : '#FAFAFA'}
+        />
       </Button>
     </Product>
   );
