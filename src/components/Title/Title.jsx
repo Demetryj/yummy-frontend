08501056165293cc027bd4ff8663cdecef9b4e4c
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-05-21 21:22:17 +0300

    delete component

--- src/components/Title/Title.jsx
+++
@@ -1,9 +0,0 @@
-import { TitleConteiner } from './Title.styled';
-
-export const Title = ({ children }) => {
-  return (
-    <TitleConteiner>
-      <h2>{children}</h2>
-    </TitleConteiner>
-  );
-};
