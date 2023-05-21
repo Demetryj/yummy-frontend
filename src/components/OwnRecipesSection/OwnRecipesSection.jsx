69e3755dcf0b1480d73cbeaa3d59075c60b95e37
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-05-21 23:10:44 +0300

    Update OwnRecipesSection.jsx

--- src/components/OwnRecipesSection/OwnRecipesSection.jsx
+++ src/components/OwnRecipesSection/OwnRecipesSection.jsx
@@ -1,10 +1,12 @@
-import { Container } from 'components/Container/Container';
-import { EmptyPagePlug } from 'components/EmptyPagePlug/EmptyPagePlug';
+import { useSelector } from 'react-redux';
+import { selectTheme } from 'redux/theme/selectors';
+import { Container } from 'components/Container';
+import { EmptyPagePlug } from 'components/EmptyPagePlug';
 import { ContentWrapper, Wrapper } from 'components/Favorites/Favorites.styled';
 import { Loader } from 'components/Loader';
-import { PaginationComp } from 'components/Pagination/Pagination';
-import { RecipeItem } from 'components/Recipe/RecipeItem';
-import { Title } from 'components/Title/Title';
+import { PaginationComp } from 'components/Pagination';
+import { RecipeItem } from 'components/Recipe';
+import { MainTitle } from 'components/MainTitle';
 
 export const OwnRecipesSection = ({
   recipes,
@@ -17,40 +19,45 @@ export const OwnRecipesSection = ({
   limit,
   handleChange,
   isLoading,
-}) => (
-  <Wrapper>
-    <Container>
-      <Title>{titleText}</Title>
-      {isLoading && <Loader />}
-      <>
-        {recipes && recipes.length > 0 ? (
-          <ContentWrapper>
-            {recipes.map(item => {
-              return (
-                <li key={item._id}>
-                  <RecipeItem
-                    location={location}
-                    id={item._id}
-                    img={item.preview ?? img}
-                    title={item.title ?? 'No name'}
-                    text={<span>{item.description ?? 'No description'}</span>}
-                    time={item.time ? `${item.time} min` : ''}
-                  />
-                </li>
-              );
-            })}
-          </ContentWrapper>
-        ) : (
-          <EmptyPagePlug location={location} text={emptyPageText} />
-        )}
-        {recipes && recipes.length > 0 && (
-          <PaginationComp
-            count={Math.ceil(total / limit)}
-            page={page}
-            handleChange={handleChange}
-          />
-        )}
-      </>
-    </Container>
-  </Wrapper>
-);
+}) => {
+  const theme = useSelector(selectTheme);
+
+  return (
+    <Wrapper>
+      <Container>
+        <MainTitle title={titleText} />
+        {isLoading && <Loader />}
+        <>
+          {recipes && recipes.length > 0 ? (
+            <ContentWrapper>
+              {recipes.map(item => {
+                return (
+                  <li key={item._id}>
+                    <RecipeItem
+                      location={location}
+                      id={item._id}
+                      img={item.preview ?? img}
+                      title={item.title ?? 'No name'}
+                      text={<span>{item.description ?? 'No description'}</span>}
+                      time={item.time ? `${item.time} min` : ''}
+                      mode={theme}
+                    />
+                  </li>
+                );
+              })}
+            </ContentWrapper>
+          ) : (
+            <EmptyPagePlug location={location} text={emptyPageText} />
+          )}
+          {recipes && recipes.length > 0 && (
+            <PaginationComp
+              count={Math.ceil(total / limit)}
+              page={page}
+              handleChange={handleChange}
+            />
+          )}
+        </>
+      </Container>
+    </Wrapper>
+  );
+};
